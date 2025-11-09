1b0325399fb7955d9d432876c5f4f5f227de2dda
taliamuro <taliamuro3@gmail.com>
2025-11-09 15:15:22 -0500

    Made changes to colors class

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color.fromRGBO(44, 44, 44, 100);
+  static const Color darkBackground = Color.fromRGBO(44, 44, 44, 100);
   static const Color accentGold = Color.fromRGBO(236, 185, 102, 100);
   static const Color accentGoldHover = Color.fromRGBO(188, 157, 109, 100);
   static const Color accentGray = Color.fromRGBO(234, 235, 238, 100);
   static const Color accentGrayHover = Color.fromRGBO(204, 205, 208, 100);
-  static const Color softWhite = Color.fromRGBO(250, 249, 246, 100);
+  static const Color lightBackground = Color.fromRGBO(250, 249, 246, 100);
 }
