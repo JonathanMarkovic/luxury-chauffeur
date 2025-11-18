0f992c0c634658b148d5598cc7e30d16ba789092
taliamuro <taliamuro3@gmail.com>
2025-11-18 16:01:16 -0500

    Added login and registration screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,33 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:luxury_chauffeur/app_colors.dart';
+import 'package:luxury_chauffeur/main.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
+void main() {
+  runApp(App());
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return MaterialApp(
+      home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class RegistrationScreen extends StatefulWidget {
-  const RegistrationScreen({super.key});
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  @override
-  State<RegistrationScreen> createState() => _RegistrationScreenState();
-}
-
-class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.darkBackground,
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Login', style: TextStyle(fontSize: 24)),
+                        TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(labelText: 'Email'),
+                        ),
+                        TextField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(labelText: 'Password'),
+                        ),
+                        SizedBox(height: 20,),
+                        ElevatedButton(
+                          onPressed: () {
 
+                          },
+                          child: Text('Login', style: TextStyle(fontSize: 18),),
+                          style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all(AppColors.darkBackground),
+                              foregroundColor: WidgetStateProperty.all(AppColors.lightBackground),
+                              shape: WidgetStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                              fixedSize: WidgetStateProperty.all(Size(120, 30))
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+
+
+class RegisterScreen extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.darkBackground,
+      body: Padding(
+        padding: EdgeInsets.all(30),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(50),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text('Registration', style: TextStyle(fontSize: 24)),
+                      TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(labelText: 'First Name'),
+                      ),
+                      TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(labelText: 'Last Name'),
+                      ),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(labelText: 'Phone Number'),
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 20,),
+                      ElevatedButton(
+                        onPressed: () {
+
+                        },
+                        child: Text('Register', style: TextStyle(fontSize: 18),),
+                        style: ButtonStyle(
+                            backgroundColor: WidgetStateProperty.all(AppColors.darkBackground),
+                            foregroundColor: WidgetStateProperty.all(AppColors.lightBackground),
+                            shape: WidgetStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                            fixedSize: WidgetStateProperty.all(Size(120, 30))
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
