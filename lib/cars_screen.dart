3bb66dd215692cfb4e12fccff11acd86f4bdc781
taliamuro <taliamuro3@gmail.com>
2025-11-18 14:50:21 -0500

    Added the cars list page

--- lib/cars_screen.dart
+++ lib/cars_screen.dart
@@ -11,7 +11,49 @@ class _CarsScreenState extends State<CarsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text('Cars'),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image(image: AssetImage('jeep.jpg')),
+                ListTile(
+                  title: Text('Jeep'),
+                  trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+                )              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image(image: AssetImage('audi.jpg')),
+                ListTile(
+                  title: Text('Audi'),
+                  trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+                )
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image(image: AssetImage('range_rover.jpg')),
+                ListTile(
+                  title: Text('Range Rover'),
+                  trailing: IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
+                )              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
