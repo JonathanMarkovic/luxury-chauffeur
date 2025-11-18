2b3920af5e46b7d6e15cff9f0124e13a35c65dc1
taliamuro <taliamuro3@gmail.com>
2025-11-18 14:22:45 -0500

    Removed home screen and changed navigation bar

--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:luxury_chauffeur/account_screen.dart';
 import 'package:luxury_chauffeur/app_colors.dart';
 import 'package:luxury_chauffeur/cars_screen.dart';
-import 'package:luxury_chauffeur/home_screen.dart';
 import 'package:luxury_chauffeur/notification_screen.dart';
 import 'package:luxury_chauffeur/reservation_screen.dart';
 
@@ -28,10 +27,9 @@ class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample>
   int _selectedIndex = 0;
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
+    NotificationScreen(),
     CarsScreen(),
     ReservationScreen(),
-    NotificationScreen(),
     AccountScreen()
   ];
 
@@ -53,11 +51,10 @@ class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample>
           type: BottomNavigationBarType.fixed,
           elevation: 0,
           items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
             BottomNavigationBarItem(icon: Icon(Icons.directions_car_filled), label: 'Cars'),
             BottomNavigationBarItem(icon: Icon(Icons.event_available_sharp), label: 'Reservations'),
-            BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: 'Notifications'),
-            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Account'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Account'),
           ],
           currentIndex: _selectedIndex,
           selectedItemColor: AppColors.accentGoldHover,
